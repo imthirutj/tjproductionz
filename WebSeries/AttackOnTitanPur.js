c132d3b74ae3bae919fd669a21b65179137f555b
imthirutj <56080056+imthirutj@users.noreply.github.com>
2021-07-06 15:24:12 +0530

    Update AttackOnTitanPur.js

--- WebSeries/AttackOnTitanPur.js
+++ WebSeries/AttackOnTitanPur.js
@@ -2,3 +2,5 @@
           pur[1]="https://m.media-amazon.com/images/M/MV5BOTIwY2VhOTYtNDY4Ni00ZDYwLTllNzItZGZlNjU5YTNlMzkzXkEyXkFqcGdeQXVyODc5MTI0NjU@._V1_UY126_CR14,0,224,126_AL_.jpg";
           pur[2]="https://m.media-amazon.com/images/M/MV5BOTlmNDZlMmQtYWNlMy00YzBjLWFkMjUtMDA0NGMyMTU4YjE2XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UY126_CR0,0,224,126_AL_.jpg";
           pur[3]="https://m.media-amazon.com/images/M/MV5BNzkwMDA0ZmItZTNjNC00YTBiLTk1ZjAtOGI5MWYzMGNmMmY4XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UX224_CR0,0,224,126_AL_.jpg";
+          pur[4]="https://m.media-amazon.com/images/M/MV5BMmZlMDgxMTYtYWY1YS00N2I3LTg5MDEtOGUxODljNDEzODc3XkEyXkFqcGdeQXVyNDY1NDU2NjM@._V1_UY126_UX224_AL_.jpg"
+          pur[5]="https://m.media-amazon.com/images/M/MV5BNmNjM2EwYTMtMGM4NC00OGRhLThkNTMtNDVlNTkwMmZjNmIwXkEyXkFqcGdeQXVyNDk2MTUxNDc@._V1_UY126_CR2,0,224,126_AL_.jpg";
