feb54077a1a976a31c6d2a9eb3538e35cf5ddf59
imthirutj <56080056+imthirutj@users.noreply.github.com>
2021-07-06 14:38:55 +0530

    Create AttackOnTitanPur.js

---
+++ WebSeries/AttackOnTitanPur.js
@@ -0,0 +1,4 @@
+          var pur=[];
+          pur[1]="https://m.media-amazon.com/images/M/MV5BOTIwY2VhOTYtNDY4Ni00ZDYwLTllNzItZGZlNjU5YTNlMzkzXkEyXkFqcGdeQXVyODc5MTI0NjU@._V1_UY126_CR14,0,224,126_AL_.jpg";
+          pur[2]="https://m.media-amazon.com/images/M/MV5BOTlmNDZlMmQtYWNlMy00YzBjLWFkMjUtMDA0NGMyMTU4YjE2XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UY126_CR0,0,224,126_AL_.jpg";
+          pur[3]="https://m.media-amazon.com/images/M/MV5BNzkwMDA0ZmItZTNjNC00YTBiLTk1ZjAtOGI5MWYzMGNmMmY4XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UX224_CR0,0,224,126_AL_.jpg";
