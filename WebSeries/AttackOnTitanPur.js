e5497f06f0a010b46108dfc6648b6128e9fef7c5
imthirutj <56080056+imthirutj@users.noreply.github.com>
2021-07-06 17:51:13 +0530

    Update AttackOnTitanPur.js

--- WebSeries/AttackOnTitanPur.js
+++ WebSeries/AttackOnTitanPur.js
@@ -1,6 +1,11 @@
-          var pur=[];
-          pur[1]="https://m.media-amazon.com/images/M/MV5BOTIwY2VhOTYtNDY4Ni00ZDYwLTllNzItZGZlNjU5YTNlMzkzXkEyXkFqcGdeQXVyODc5MTI0NjU@._V1_UY126_CR14,0,224,126_AL_.jpg";
-          pur[2]="https://m.media-amazon.com/images/M/MV5BOTlmNDZlMmQtYWNlMy00YzBjLWFkMjUtMDA0NGMyMTU4YjE2XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UY126_CR0,0,224,126_AL_.jpg";
-          pur[3]="https://m.media-amazon.com/images/M/MV5BNzkwMDA0ZmItZTNjNC00YTBiLTk1ZjAtOGI5MWYzMGNmMmY4XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UX224_CR0,0,224,126_AL_.jpg";
-          pur[4]="https://m.media-amazon.com/images/M/MV5BMmZlMDgxMTYtYWY1YS00N2I3LTg5MDEtOGUxODljNDEzODc3XkEyXkFqcGdeQXVyNDY1NDU2NjM@._V1_UY126_UX224_AL_.jpg"
-          pur[5]="https://m.media-amazon.com/images/M/MV5BNmNjM2EwYTMtMGM4NC00OGRhLThkNTMtNDVlNTkwMmZjNmIwXkEyXkFqcGdeQXVyNDk2MTUxNDc@._V1_UY126_CR2,0,224,126_AL_.jpg";
+          var s1pur=[];
+          s1pur[1]="https://m.media-amazon.com/images/M/MV5BOTIwY2VhOTYtNDY4Ni00ZDYwLTllNzItZGZlNjU5YTNlMzkzXkEyXkFqcGdeQXVyODc5MTI0NjU@._V1_UY126_CR14,0,224,126_AL_.jpg";
+          s1pur[2]="https://m.media-amazon.com/images/M/MV5BOTlmNDZlMmQtYWNlMy00YzBjLWFkMjUtMDA0NGMyMTU4YjE2XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UY126_CR0,0,224,126_AL_.jpg";
+          s1pur[3]="https://m.media-amazon.com/images/M/MV5BNzkwMDA0ZmItZTNjNC00YTBiLTk1ZjAtOGI5MWYzMGNmMmY4XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UX224_CR0,0,224,126_AL_.jpg";
+          s1pur[4]="https://m.media-amazon.com/images/M/MV5BMmZlMDgxMTYtYWY1YS00N2I3LTg5MDEtOGUxODljNDEzODc3XkEyXkFqcGdeQXVyNDY1NDU2NjM@._V1_UY126_UX224_AL_.jpg"
+          s1pur[5]="https://m.media-amazon.com/images/M/MV5BNmNjM2EwYTMtMGM4NC00OGRhLThkNTMtNDVlNTkwMmZjNmIwXkEyXkFqcGdeQXVyNDk2MTUxNDc@._V1_UY126_CR2,0,224,126_AL_.jpg";
+       
+
+
+var s2pur=[];
+s2pur[1]="https://m.media-amazon.com/images/M/MV5BMDJkNTQwODctZTIyZS00N2Q1LWI3YjAtOTY0MjllNGU3OGNmXkEyXkFqcGdeQXVyODc5MTI0NjU@._V1_UY126_UX224_AL_.jpg";
