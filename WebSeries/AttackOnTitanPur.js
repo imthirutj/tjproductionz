a7e614479551053641161c5011b0f7e30c9a32e3
imthirutj <56080056+imthirutj@users.noreply.github.com>
2021-07-09 13:28:43 +0530

    Update AttackOnTitanPur.js

--- WebSeries/AttackOnTitanPur.js
+++ WebSeries/AttackOnTitanPur.js
@@ -1,10 +1,21 @@
-          var s1pur=[];
-          s1pur[1]="https://m.media-amazon.com/images/M/MV5BOTIwY2VhOTYtNDY4Ni00ZDYwLTllNzItZGZlNjU5YTNlMzkzXkEyXkFqcGdeQXVyODc5MTI0NjU@._V1_UY126_CR14,0,224,126_AL_.jpg";
-          s1pur[2]="https://m.media-amazon.com/images/M/MV5BOTlmNDZlMmQtYWNlMy00YzBjLWFkMjUtMDA0NGMyMTU4YjE2XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UY126_CR0,0,224,126_AL_.jpg";
-          s1pur[3]="https://m.media-amazon.com/images/M/MV5BNzkwMDA0ZmItZTNjNC00YTBiLTk1ZjAtOGI5MWYzMGNmMmY4XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UX224_CR0,0,224,126_AL_.jpg";
-          s1pur[4]="https://m.media-amazon.com/images/M/MV5BMmZlMDgxMTYtYWY1YS00N2I3LTg5MDEtOGUxODljNDEzODc3XkEyXkFqcGdeQXVyNDY1NDU2NjM@._V1_UY126_UX224_AL_.jpg"
-          s1pur[5]="https://m.media-amazon.com/images/M/MV5BNmNjM2EwYTMtMGM4NC00OGRhLThkNTMtNDVlNTkwMmZjNmIwXkEyXkFqcGdeQXVyNDk2MTUxNDc@._V1_UY126_CR2,0,224,126_AL_.jpg";
+var s1pur=[];
+s1pur[1]="https://m.media-amazon.com/images/M/MV5BOTIwY2VhOTYtNDY4Ni00ZDYwLTllNzItZGZlNjU5YTNlMzkzXkEyXkFqcGdeQXVyODc5MTI0NjU@._V1_UY126_CR14,0,224,126_AL_.jpg";
+s1pur[2]="https://m.media-amazon.com/images/M/MV5BOTlmNDZlMmQtYWNlMy00YzBjLWFkMjUtMDA0NGMyMTU4YjE2XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UY126_CR0,0,224,126_AL_.jpg";
+s1pur[3]="https://m.media-amazon.com/images/M/MV5BNzkwMDA0ZmItZTNjNC00YTBiLTk1ZjAtOGI5MWYzMGNmMmY4XkEyXkFqcGdeQXVyMzExMzk5MTQ@._V1_UX224_CR0,0,224,126_AL_.jpg";
+s1pur[4]="https://m.media-amazon.com/images/M/MV5BMmZlMDgxMTYtYWY1YS00N2I3LTg5MDEtOGUxODljNDEzODc3XkEyXkFqcGdeQXVyNDY1NDU2NjM@._V1_UY126_UX224_AL_.jpg"
+s1pur[5]="https://m.media-amazon.com/images/M/MV5BNmNjM2EwYTMtMGM4NC00OGRhLThkNTMtNDVlNTkwMmZjNmIwXkEyXkFqcGdeQXVyNDk2MTUxNDc@._V1_UY126_CR2,0,224,126_AL_.jpg";
+
+
+
        
+
+
+
+
+var s1src=[];
+s1src[1]="https://store9.gofile.io/download/99c4e432-42ba-4f3d-893f-014e04a8dbd5/9animez.com%20-%20Attack%20on%20Titan%20-%20S01E01%20To%20You%2C%20Two%20Thousand%20Years%20Later%20%5B480P%5D%5BDual-Audio%5D.mkv";
+s1src[2]="https://store9.gofile.io/download/26cb03a9-4246-4c36-8034-3dc9d475752d/9animez.com%20-%20Attack%20on%20Titan%20-%20S01E02%20That%20Day%20%5B480P%5D%5BDual-Audio%5D.mkv";
+
 
 
 var s2pur=[];
